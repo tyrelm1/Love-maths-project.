4a64476b1273f8ead51852326ea2b9f6887693fa
Tyrel Morant <tyrel_m@hotmail.co.uk>
2024-01-29 13:39:09 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,46 @@
-function runGame() {
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+function runGame() {
 
 }
 
 function checkAnswer() {
 
-
 }
 
 function calculateCorrectAnswer() {
-
 
 }
 
 function incrementScore() {
 
+}
+
+function incrementWrongAnswer() {
 
 }
 
-function incrementWrongAnswer(){
-
-
-}
-
-function displayAdttionQuestion() {
-
+function displayAdditionQuestion() {
 
 }
 
 function displaySubtractQuestion() {
 
-
 }
 
 function displayMultiplyQuestion() {
-
     
 }
