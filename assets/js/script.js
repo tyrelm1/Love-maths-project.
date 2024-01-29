cfcbf71750fc8a06321f04f21634c502728524b7
Tyrel Morant <tyrel_m@hotmail.co.uk>
2024-01-29 13:14:33 +0000

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+function runGame() {
+
+
+}
+
+function checkAnswer() {
+
+
+}
+
+function calculateCorrectAnswer() {
+
+
+}
+
+function incrementScore() {
+
+
+}
+
+function incrementWrongAnswer(){
+
+
+}
+
+function displayAdttionQuestion() {
+
+
+}
+
+function displaySubtractQuestion() {
+
+
+}
+
+function displayMultiplyQuestion() {
+
+    
+}
